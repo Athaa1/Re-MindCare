9df5f0d1a7bddff1cb821fcf3e0a31e81f4033d5
MUHAMMAD ATHAYA FATHIN ADANI - <23523008@students.uii.ac.id>
2025-07-02 12:50:26 +0000

    buatkan juga di dokter

--- src/components/doctor-dashboard/RecentPatients.tsx
+++ src/components/doctor-dashboard/RecentPatients.tsx
@@ -1,4 +1,3 @@
-
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
